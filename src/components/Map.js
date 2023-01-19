d22ac987beb38927951458ccd91400da72bca51d
Hans Sandbu <hans.sandbu@geodata.no>
2023-01-19 08:40:49 +0100

    Added step 3

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,6 +3,7 @@ import React, { useRef, useEffect, useContext } from "react";
 import esriConfig from "@arcgis/core/config.js";
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
+import Locate from "@arcgis/core/widgets/Locate";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 import { AppContext } from "../state/context";
@@ -38,6 +39,7 @@ const MapComponent = () => {
           }]
         }
       });
+
       // Legg til dataen i kartet
       map.add(featureLayer);
 
@@ -63,12 +65,37 @@ const MapComponent = () => {
         // Dokumentasjon for dette er på:
         // https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Locate.html
         // En god idé er å sette zoom nivået med scale attributten, f. eks scale 5000
+        var locateWidget = new Locate({
+          view: mapView,
+          scale: 5000,
+        });
+        mapView.ui.add(locateWidget, "top-left");
+
         // Vi har også lagd en egen widget som kan legges til i kartet
         // Denne ligger i components/RouteWidget, men den er ikke helt ferdig enda
         // Den må først legges til i App.js fila, men det er noen feil med den
-        // Det første vi må gjøre er å sørge for at Widgeten har tilgang til MapViewet
+
+        // Det første vi må gjøre er å sørge for at widgeten har tilgang til MapViewet
         // Dette kan gjøres ved å legge den til i contexten vår
         // Resten av feilene må løses i RouteWidget fila
+        context.mapView.set(mapView);
+
+        // Til slutt ønsker vi at brukeren skal kunne velge startssted for widget selv
+        // Dette kan gjøres ved å bruke lokasjonen fra locate widgeten, eller ved å f. eks. klikke i kartet
+        // For å kunne bruke locate widgeten trenger vi en lytter på widgeten.
+        // Mer om dette finnes på siden:
+        // https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Locate.html#on
+        // Det er mulig å lagre resultatet fra widgeten i contexten med context.point.set()
+
+        // For å kunne klikke i kartet trenger vi en lytter på MapViewet
+        // Dette gjøres ganske likt som for locate widgeten, og mer info er på API siden for MapView
+
+        // For klikk i kart er det også et ønsket om å legge til et punkt som viser hvor brukeren har klikket
+        // For å gjøre dette trenger vi å ta i bruk noe for symbologi, f. eks. SimpleMarkerSymbol
+        // https://developers.arcgis.com/javascript/latest/api-reference/esri-symbols-SimpleMarkerSymbol.html
+        // Og vi trenger en grafikk som legges til MapView
+        // https://developers.arcgis.com/javascript/latest/api-reference/esri-Graphic.html
+        // hint: se på koden i RouteWidget
       });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
--- src/components/RouteWidget.js
+++ src/components/RouteWidget.js
@@ -11,20 +11,6 @@ const RouteWidget = () => {
   const [length, setLength] = useState(0); // Lengde på kalkulert rute
   const [radius, setRadius] = useState(1.5); // Radius rundt brukerens posisjon
   const isMobile = navigator.userAgent.match(/Mobile|Windows Phone|Lumia|Android|webOS|iPhone|iPod|Blackberry|PlayBook|BB10|Opera Mini|\bCrMo\/|Opera Mobi/i)
-  const radiusToLengthOptions = [
-    {
-      radius: 0.5,
-      meter: 3000
-    },
-    {
-      radius: 0.7,
-      meter: 5000
-    },
-    {
-      radius: 1.5,
-      meter: 10000
-    }
-  ];
 
   // Kalkuler ca antall skritt basert på rutens lengde
   const getSteps = (length) => {
@@ -39,7 +25,8 @@ const RouteWidget = () => {
       // Vi trenger å fjerne gamle ruter før vi legger til nye
       // Hint: grafikken legges til MapView 
       const mapView = context.mapView.value;
-
+      const oldLine = mapView.graphics.items.filter((item) => { return item.geometry.type === "polyline" })[0];
+      mapView.graphics.remove(oldLine);
       const route = result.data.routeResults[0].route;
 
       route.attributes.name = "route";
@@ -58,16 +45,10 @@ const RouteWidget = () => {
     <div className={isMobile ? "widgetContainerMobile" : "widgetContainer"}>
       Hvor mange skritt vil du gå idag?
       <div style={{ margin: "20px" }}>
-        {/*
-					Her har vi allerede brukt TextField componentet fra Material UI. 
-					Material UI er en bibliotek med mange nyttige React komponenter vi kan bruke. 
-				*/}
         <TextField id="select" label="Skritt" value={radius} select onChange={(e) => setRadius(e.target.value)}>
-          {/* 
-						Vi ønsker å gi brukeren noen valg over hvor mange skritt som skal gås pr. rute 
-						Her kan vi bruke MenuItem inni TextField komponenten for å vise en dropdown.
-						Dokumentasjonen for det er her: https://mui.com/material-ui/react-text-field/
-					*/}
+          <MenuItem value={0.5}>3000</MenuItem>
+          <MenuItem value={0.7}>5000</MenuItem>
+          <MenuItem value={1.5}>10000</MenuItem>
         </TextField>
       </div>
       <Button variant="contained" color="primary" onClick={() => getRoute()}>
