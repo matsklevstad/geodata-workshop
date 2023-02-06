d2f063929dbd2bb7deca2cd091dade83d8077eeb
Marie Falch Orre <marie.orre@geodata.no>
2023-02-06 15:24:43 +0100

    Fixed task 1

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,63 +10,62 @@ import PopupTemplate from "@arcgis/core/PopupTemplate";
 import "../App.css";
 
 const MapComponent = () => {
-  // Required: Set this property to insure assets resolve correctly.
   esriConfig.assetsPath = "./assets";
-  const mapDiv = useRef(null);
   const context = useContext(AppContext);
 
-  // Opprett kartet
   useEffect(() => {
-    if (mapDiv.current) {
-      // Det første vi trenger er et Map objekt med bakgrunnskart
-      // Konstruktøren er allerede i koden, men vi må velge bakgrunnskartet
-      // En liste med valg finner vi i API dokumentasjonen:
-      // https://developers.arcgis.com/javascript/latest/api-reference/esri-Map.html#basemap
-      const map = new Map({
-        basemap: "",
-      });
+    // Det første vi trenger er et Map objekt med bakgrunnskart
+    // Konstruktøren er allerede i koden, men vi må velge bakgrunnskartet
+    // En liste med valg finner vi i API dokumentasjonen under "For use without an API key":
+    // https://developers.arcgis.com/javascript/latest/api-reference/esri-Map.html#basemap
 
-      // Vi ønsker så å hente data som vi kan legge til i kartet.
-      // På følgende tjeneste finner dere punkter som viser en rekke turistatraksjoner i Europa:
-      // Url: https://services-eu1.arcgis.com/zci5bUiJ8olAal7N/arcgis/rest/services/OSM_Tourism_EU/FeatureServer/0
-      // Se dokumentasjonssiden for et eksempel: https://developers.arcgis.com/javascript/latest/add-a-feature-layer/
+    const map = new Map({
+      // TODO: Definer bakgrunnskart
+      basemap: "",
+    });
 
-      // Om man ønsker å ha en popup når man trykker på punktene i FeatureLayeret trenger man også å legge
-      // inn en popup template i featurelayeret. Denne har vi ferdig definert her.
-      const popUpTemplate = new PopupTemplate({
-        title: "{name}",
-        content: [{
+    // Vi ønsker så å hente data som vi kan legge til i kartet.
+    // På følgende tjeneste finner dere punkter som viser en rekke turistatraksjoner i Europa:
+    // Url: https://services-eu1.arcgis.com/zci5bUiJ8olAal7N/arcgis/rest/services/OSM_Tourism_EU/FeatureServer/0
+    // Se dokumentasjonssiden for et eksempel: https://developers.arcgis.com/javascript/latest/add-a-feature-layer/
+
+    // TODO: Legge hente data
+    // TODO: Legge til dataen i kartet
+    // TODO: Legg til dataen i context
+
+    // Om man ønsker å ha en popup når man trykker på punktene i FeatureLayeret trenger man også å legge
+    // inn en popup template i featurelayeret. Denne har vi ferdig definert her.
+    const popUpTemplate = new PopupTemplate({
+      title: "{name}",
+      content: [
+        {
           type: "text",
           text: `<p>Type: {tourism}</p>
-                  <p>{description}</p>`
-        }]
-      });
+                  <p>{description}</p>`,
+        },
+      ],
+    });
 
-      // TODO: Legge hente data
-      // TODO: Legge til dataen i kartet
-      // TODO: Legg til dataen i context
+    // For å kunne vise kartet må dette legges til i et MapView
+    // Dokumentasjonen for MapView finnes her:
+    // https://developers.arcgis.com/javascript/latest/api-reference/esri-views-MapView.html
+    // Koorinater for Trondheimsområdet:
+    // ymin: 63.40182257265643,
+    // xmin: 10.227928161621094,
+    // ymax: 63.453731595863324,
+    // xmax: 10.560264587402344
+    new MapView({
+      // map: ,
+      // container: , // Referanse til DOM elementets ID
+      // extent:
+    }).when(() => {
+      // Denne koden kjører når karter er oppe å går
+    });
 
-      // For å kunne vise kartet må dette legges til i et MapView
-      // Dokumentasjonen for MapView finnes her:
-      // https://developers.arcgis.com/javascript/latest/api-reference/esri-views-MapView.html
-      // Dersom man ønsker å se Trondheimsområdet er koordinatene for det
-      // ymin: 63.40182257265643,
-      // xmin: 10.227928161621094,
-      // ymax: 63.453731595863324,
-      // xmax: 10.560264587402344
-      new MapView({
-        // MapView trenger minimum feltene:
-        // map
-        // container
-        // extent(valgfritt, men lurt å ha med)
-      }).when(() => {
-        // Når kartet er initialisert kan vi manipulre dataen her
-      });
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div className="mapDiv" ref={mapDiv}></div>;
+  return <div id="mapDiv" style={{ height: "100%", width: "100%" }}></div>;
 };
 
 export default MapComponent;
